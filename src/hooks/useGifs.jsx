accb634cc0811cd1a0b971a3e63b5f08bc63aacd
Aracely Alba <aracelyalba8@gmail.com>
2023-02-28 23:36:19 -0430

    Added Infinite Scroll

--- src/hooks/useGifs.jsx
+++ src/hooks/useGifs.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 import getGifs from './../services/getGifs.jsx';
 
+const INITIAL_PAGE = 0;
+
 export function useGifs({ keyword } = { keyword: null }) {
 	
 	const [loading, setLoading] = useState(false);
+	const [loadingNextPage, setLoadingNextPage] = useState(false);
 	const [gifs, setGifs] = useState([]);
+	const [page, setPage] = useState(INITIAL_PAGE);
+
+	const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
 
 	useEffect(() => {
 
 		setLoading(true);
-
-		const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
 
 		getGifs({ keyword: keywordToUse })
 			.then(gifs => {
@@ -20,7 +24,23 @@ export function useGifs({ keyword } = { keyword: null }) {
 				localStorage.setItem('lastKeyword', keyword);
 			});
 
-	}, [keyword]);
+	}, [keyword, keywordToUse, setGifs]);
 
-	return { loading, gifs };
+
+	useEffect(() => {
+
+		if(page === INITIAL_PAGE ) return;
+
+		setLoadingNextPage(true);
+
+		getGifs({ keyword: keywordToUse, page})
+			.then(nextGifs => { 
+
+				setGifs(prevGifs => prevGifs.concat(nextGifs));
+				setLoadingNextPage(false);
+			});
+
+	}, [page, keywordToUse, setGifs]);
+
+	return { loading, loadingNextPage, gifs, setPage };
 };
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
+import debounce from 'just-debounce-it';
+import Spinner from './../components/Spinner';
 import CardsList from './../components/CardsList.jsx';
 import { useGifs } from './../hooks/useGifs.jsx';
-
-import Spinner from './../components/Spinner';
+import { useNearScreen } from './../hooks/useNearScreen.jsx';
 
 const SearchResults = ({ params }) => {
 	
 	const { keyword } = params;
-	const { loading, gifs } = useGifs({ keyword });
+	const { loading, loadingNextPage, gifs, setPage } = useGifs({ keyword });
+	const externalRef = useRef();
+	const { isNearScreen } = useNearScreen({ externalRef : loading ? null : externalRef, once: false });
+
+	const debounceHandleNextPage = useCallback(debounce(() => setPage(prevPage => prevPage + 1), 200), [setPage]);
+
+	useEffect(() => {
+
+		if(isNearScreen) {
+
+			debounceHandleNextPage();
+		}
+
+	}, [isNearScreen, debounceHandleNextPage]);
 
 	return (
 
@@ -15,7 +29,10 @@ const SearchResults = ({ params }) => {
 			<h2 className="container--title">{ decodeURI(keyword) }</h2>
 			{ loading
 					? <Spinner/>
-					: <CardsList gifs={gifs} />
+					: <>
+							<CardsList gifs={gifs} />
+							<div id="watcher" ref={externalRef}></div>
+						</>
 			}
 		</section>
 		
